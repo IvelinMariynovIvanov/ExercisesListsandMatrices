679c70d66c6055735382c385c3edbeb8da81a08f
IvelinMariyanovIvanov <i.ivanov_1987@abv.bg>
2017-02-19 22:59:36 -0800

    a

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesListsandMatrices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            int lenght = 1;
+            int maxlenght = 1;
+            int numb = 0;
+
+            for (int i = 0; i < nums.Count-1; i++)
+            {
+                
+
+                if (nums[i] == nums[i+1])
+                {
+                    lenght++;
+                }
+                else
+                {
+                    if(lenght > maxlenght)
+                    {
+                        maxlenght = lenght;
+                        numb = nums[i];
+                    }
+                    lenght = 1;
+                    
+                }
+            }
+            for (int i = 0; i < maxlenght; i++)
+            {
+                Console.Write(numb + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
